7eaf563c88d4c175ade4c3bdd375f180b323b39b
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-05-19 16:42:31 -0300

    resolve o exercicio maior de 3

---
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao.1/maiorde3.js
@@ -0,0 +1,11 @@
+let a = 5
+let b = 5
+let c = 5
+
+
+if (a>b & a>c) {console.log("a é o maior")}
+else if (a==b & a==c) {console.log("são iguais")}
+else if (b>a & b>c) {console.log("b é o maior")}
+else if (c>a & c>b) {console.log("c é o maior")}
+else {console.log("erro")}
+
