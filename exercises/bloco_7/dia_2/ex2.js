f051657d6ed487af7675ff6f8d69ec5b576abad0
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-06-18 18:07:24 -0300

    resolve os ultimos ex da parte II

--- exercises/bloco_7/dia_2/ex2.js
+++ exercises/bloco_7/dia_2/ex2.js
@@ -28,6 +28,39 @@ const listKeys = (object) => Object.keys(object);
 
 const objLength = (object) => Object.keys(object).length;
 
+const objValues = (object) => Object.values(object);
+
 // addTurn(lesson2, 'turno', 'manhã');
 
-console.log(objLength(lesson2));
+// let allLessons = { lesson1, lesson2, lesson3 }; // Para juntar objetos dentro de um objeto.
+
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
+
+const contaAlunos = (objeto) => {
+  let contador = 0;
+  for (let index = 0; index < Object.keys(objeto).length; index += 1) {
+    contador += objeto[Object.keys(objeto)[index]].numeroEstudantes;
+  }
+  return contador;
+};
+
+console.log(contaAlunos(allLessons));
+
+const obtemValor = (objeto, posição) => {
+  let key = Object.values(objeto)[posição];
+
+  return key;
+};
+
+const existeNoObj = (objeto, chave, valor) => {
+  let answer;
+  if (objeto[chave] === valor) {
+    answer = true;
+  } else {
+    answer = false;
+  }
+
+  return answer;
+};
+
+console.log(existeNoObj(lesson3, 'turno', 'manha'));
