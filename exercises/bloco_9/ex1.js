51da88597670ec3cca91160b7dc8ffd8b2f071c3
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-07-06 16:04:45 -0300

    resolve todos os exercicio de fixação e ex.

---
+++ exercises/bloco_9/ex1.js
@@ -0,0 +1,32 @@
+//Dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?
+
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+ { return `${name} is ${value} ${measurementUnit} apart from the Sun`};
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // 1
+console.log(planetDistanceFromSun(venus)); // 2
+console.log(planetDistanceFromSun(jupiter)); // 3
