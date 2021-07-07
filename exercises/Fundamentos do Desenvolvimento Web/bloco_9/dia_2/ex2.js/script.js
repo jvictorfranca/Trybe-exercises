5138bfda36c8145ac7934132e61ae5efa136cf3e
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-07-07 19:53:36 -0300

    resolve os ex e adiciona teste

--- exercises/Fundamentos do Desenvolvimento Web/bloco_9/dia_2/ex2.js/script.js
+++ exercises/Fundamentos do Desenvolvimento Web/bloco_9/dia_2/ex2.js/script.js
@@ -30,3 +30,35 @@ function testPromise() {
 }
 
 testPromise();
+
+// TESTE:
+// async function testPromise() {
+//   const myPromise = new Promise((resolve, reject) => {
+//     let array = [];
+//     for (let index = 0; index < 10; index += 1) {
+//       let number = Math.floor(Math.random() * 50) + 1;
+//       number = number ** 2;
+//       array.push(number);
+//     }
+//     let sum = array.reduce((acum, curr) => acum + curr);
+//     if (sum < 8000) {
+//       resolve(sum);
+//     } else {
+//       reject();
+//     }
+//   });
+//   result = await myPromise
+//     .then((sum) => {
+//       let array = [];
+//       array.push(sum / 2);
+//       array.push(sum / 3);
+//       array.push(sum / 5);
+//       array.push(sum / 10);
+//       return array;
+//     })
+//     .then((array) => array.reduce((sum, numb) => sum + numb))
+//     .catch(() => 'É mais de oito mil! Essa promise deve estar quebrada!');
+//   return result;
+// }
+
+// console.log(testPromise());
