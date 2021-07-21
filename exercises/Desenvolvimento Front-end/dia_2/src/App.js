12b5e5e3dd8a5cac19ae0805fc85de0fb947b49c
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-07-21 14:53:32 -0300

    resolve o ex do dia

--- exercises/Desenvolvimento Front-end/dia_2/src/App.js
+++ exercises/Desenvolvimento Front-end/dia_2/src/App.js
@@ -1,18 +1,11 @@
 import data from './data';
 import './App.css';
+import Pokedex from './Pokedex';
+
 
 function App() {
   return (
-    <div className="pokedex">
-     {data.map(pokemon=> <div key={pokemon.id} className = 'Pokemon'>
-     <div className='info'>
-    <p>{pokemon.name}</p>
-    <p>{pokemon.type}</p>
-    <p>Average weigth: {pokemon.averageWeight.value} {pokemon.averageWeight.measurementUnit}</p>
-       </div> 
-      <img src={pokemon.image} alt="" />
-     </div>)}
-    </div>
+    <Pokedex />
   );
 }
 
