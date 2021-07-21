77e5b621d770a29b4f8da93a3ecfbca1de55c799
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-07-21 14:20:44 -0300

    resolve o ex mas tem que refatorar

--- exercises/Desenvolvimento Front-end/dia_2/src/App.js
+++ exercises/Desenvolvimento Front-end/dia_2/src/App.js
@@ -1,10 +1,17 @@
-import data from '.data.js';
+import data from './data';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     {console.log(data)}
+    <div className="pokedex">
+     {data.map(pokemon=> <div key={pokemon.id} className = 'Pokemon'>
+     <div className='info'>
+    <p>{pokemon.name}</p>
+    <p>{pokemon.type}</p>
+    <p>Average weigth: {pokemon.averageWeight.value} {pokemon.averageWeight.measurementUnit}</p>
+       </div> 
+      <img src={pokemon.image} alt="" />
+     </div>)}
     </div>
   );
 }
